c5c362f9204e151b71c0c86f6b6f9ccf3ec48a1e
kvl00l <161270126+kvl00l@users.noreply.github.com>
2024-04-29 21:12:11 +0800

    Add files via upload

---
+++ Solution 1.py
@@ -0,0 +1,33 @@
+# Solution 1
+
+def mostFrequentWord(arr, a):
+  
+    # Freq to store the freq of the most occurring variable
+    freq = 0
+    
+    # Res to store the most occurring string in the array of strings
+    res = ""
+
+    # Nested for loops to find the most occurring
+    # Word in the array of strings
+    for i in range(0, a, 1):
+        count = 1
+        for j in range(i + 1, a, 1):
+            if arr[j] == arr[i]:
+                count += 1 
+
+        # Updating our max freq of occurred string in the
+        # Array of strings
+        if count >= freq:
+            res = arr[i]
+            freq = count
+
+    print("The word that occurs most is : " + str(res))
+    print("No of times: " + str(freq))
+
+# Set of keys
+arr = [ "new", "job", "study", "appier", "coding", "to", "appier", "intern", "new", "computer", "science", "data", "appier", "fire", "in", "be", "data", "appier",]
+n = len(arr)
+
+# function call
+mostFrequentWord(arr, n)
